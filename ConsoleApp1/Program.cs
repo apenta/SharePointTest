28035cb8bd415e90165d830c03688d303a479c2e
apenta <apenta@kent.edu>
2018-05-29 09:07:45 -0400

    Initial gitbash commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security;
+using SPClient=Microsoft.SharePoint.Client;
+using Microsoft.SharePoint;
+using Microsoft.ProjectServer.Client;
+using Microsoft.SharePoint.Client;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = "annapenta@lexmarktest.onmicrosoft.com";
+            Console.WriteLine("Enter your password.");
+            SecureString password = GetPassword();
+            // ClienContext - Get the context for the SharePoint Online Site  
+            // SharePoint site URL - https://lexmarktest.sharepoint.com 
+            using (var clientContext = new ClientContext("https://lexmarktest.sharepoint.com"))
+            {
+                // SharePoint Online Credentials  
+                clientContext.Credentials = new SharePointOnlineCredentials(userName, password);
+                // Get the SharePoint web  
+                Web web = clientContext.Web;
+                // Load the Web properties  
+                clientContext.Load(web);
+                // Execute the query to the server.  
+                clientContext.ExecuteQuery();
+                // Web properties - Display the Title and URL for the web  
+                Console.WriteLine("Title: " + web.Title + "; URL: " + web.Url);
+                //Console.ReadLine();
+
+                //var lists = web.Lists;
+                //clientContext.Load(lists);
+                //clientContext.ExecuteQuery();
+                //Console.Read();
+                //Console.WriteLine(lists);
+
+                Microsoft.SharePoint.Client.List spList = clientContext.Web.Lists.GetByTitle("List Template Gallery");
+                clientContext.Load(spList);
+                clientContext.ExecuteQuery();
+
+
+                if (spList != null && spList.ItemCount > 0)
+                {
+                    Microsoft.SharePoint.Client.CamlQuery camlQuery = new CamlQuery();
+                    camlQuery.ViewXml =
+                        @"<View>
+                            <ViewFields><FieldRef Name= 'TemplateTitle' /></ViewFields>
+                          </View>";
+
+                    ListItemCollection listItems = spList.GetItems(camlQuery);
+                    clientContext.Load(listItems);
+                    clientContext.ExecuteQuery();
+                    Console.WriteLine(listItems);
+                    Console.ReadLine();
+                }
+
+            }
+        }
+        private static SecureString GetPassword()
+        {
+            ConsoleKeyInfo info;
+            //Get the user's password as a SecureString  
+            SecureString securePassword = new SecureString();
+            do
+            {
+                info = Console.ReadKey(true);
+                if (info.Key != ConsoleKey.Enter)
+                {
+                    securePassword.AppendChar(info.KeyChar);
+                }
+            }
+            while (info.Key != ConsoleKey.Enter);
+            return securePassword;
+        }
+    }
+}
+
---
+++ LexmarkTest/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;  
+using System.Security;  
+using Microsoft.SharePoint.Client;  
+using System.Collections.Generic;  
+using System.Linq;  
+using System.Text;  
+using System.Threading.Tasks;
+
+namespace CSOMOffice365
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SharePoint Online site URL:");
+            stringwebSPOUrl = Console.ReadLine();
+            Console.WriteLine("User Name:");
+            stringuserName = Console.ReadLine();
+            Console.WriteLine("Password:");
+            SecureString password = FetchPasswordFromConsole();
+            try
+            {
+                using (var context = new ClientContext(webSPOUrl))
+                {
+                    context.Credentials = new SharePointOnlineCredentials(userName, password);
+                    Web web = context.Web;
+                    context.Load(web.Lists,
+                        lists => lists.Include(list => list.Title,
+                            list => list.Id));
+                    context.ExecuteQuery();
+                    Console.ForegroundColor = ConsoleColor.White;
+                    foreach (List list in web.Lists)
+                    {
+                        Console.WriteLine("List title is: " + list.Title);
+                    }
+                    Console.WriteLine("");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error is: " + ex.Message);
+            }
+        }
+        private static SecureStringFetchPasswordFromConsole()
+        {
+            string password = "";
+            ConsoleKeyInfo info = Console.ReadKey(true);
+            while (info.Key != ConsoleKey.Enter)
+            {
+                if (info.Key != ConsoleKey.Backspace)
+                {
+                    Console.Write("*");
+                    password += info.KeyChar;
+                }
+                else if (info.Key == ConsoleKey.Backspace)
+                {
+                    if (!string.IsNullOrEmpty(password))
+                    {
+                        password = password.Substring(0, password.Length - 1);
+                        intpos = Console.CursorLeft;
+                        Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                        Console.Write(" ");
+                        Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                    }
+                }
+                info = Console.ReadKey(true);
+            }
+            Console.WriteLine();
+            varsecurePassword = new SecureString();
+            //Convert string to secure string  
+            foreach (char c in password)
+                securePassword.AppendChar(c);
+            securePassword.MakeReadOnly();
+            returnsecurePassword;
+        }
+    }
+}
